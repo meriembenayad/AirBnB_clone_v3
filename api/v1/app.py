a184874a00df2ab4d7e9c4e8536da2b61302a2ae
ThamiBa <baladithami@gmail.com>
2024-01-07 19:21:16 +0100

    👨‍💻 Task3

---
+++ api/v1/app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+Createw Flask app; and register the blueprint app_views to Flask instance app.
+'''
+
+from os import getenv
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+# Register the app_views blueprint:
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+# Teardown function to close the SQLAlchemy Session object after each request:
+@app.teardown_appcontext
+def teardown_engine(exception):
+    '''
+    Removes the current SQLAlchemy Session object after each request.
+    '''
+    storage.close()
+
+
+# Error handlers for expected app behavior:
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    Return errmsg `Not Found`.
+    '''
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
+
+
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Create a Route `/status` on the object app_views.
+'''
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+@app_views.route('/status', methods=['GET'])
+def api_status():
+    '''
+    Returns a JSON response for RESTful API health
+    '''
+    response = {'status': 'OK'}
+    return jsonify(response)
