d84e4768003e6113b041266fd8d44616444d8af7
ThamiBa <baladithami@gmail.com>
2024-01-08 03:00:35 +0100

    👨‍💻 fix_Task4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-'''
-Create a route `/status` on the object app_views.
-'''
-
+"""
+Create a Route `/status` on the object app_views.
+"""
 
 from flask import jsonify
 from api.v1.views import app_views
@@ -11,25 +10,8 @@ from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def api_status():
-    '''
-    Returns a JSON response for RESTful API health.
-    '''
+    """
+    Returns a JSON response for RESTful API health
+    """
     response = {'status': 'OK'}
     return jsonify(response)
-
-# task 4
-
-@app_views.route('/stats', methods=['GET'])
-def get_stats():
-    '''
-    Retrieves the number of each objects by type.
-    '''
-    stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
-    }
-    return jsonify(stats)
