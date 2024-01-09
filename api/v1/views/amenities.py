3fe019fba28c7eb1bb30d18280de659450f5ebde
meriembenayad <meriem.benayad@gmail.com>
2024-01-09 13:11:42 +0100

    Task 8: Amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,40 +2,38 @@
 """
 Create a view Amenity objects that handles all default RESTFul API
 """
-from flask import jsonify, abort, request
 from api.v1.views import app_views
-from models import Amenity
+from models import storage
+from api.v1.views import app_views
+from models.amenity import Amenity
+from flask import jsonify, abort, request
 
 
-@app_views.route('/amenities', methods=['GET'])
-def get_amenities():
+@app_views.route('/amenities', strict_slashes=False, methods=['GET'])
+@app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['GET'])
+def get_amenities(amenity_id=None):
     """
         Retrieves the list of all Amenity objects:
         GET /api/v1/amenities
-    """
-    amenities = Amenity.query.all()
-    amenities_list = [Amenity.to_dict() for amenity in amenities]
-
-    return jsonify(amenities_list)
-
-
-@app_views.route('/amenities/<amenity_id>', methods=['GET'])
-def get_amenity(amenity_id):
-    """
-        Retrieves a Amenity object:
         GET /api/v1/amenities/<amenity_id>
 
         Args:
         amenity_id -- Amenity Id
     """
-    amenity = Amenity.query.get(amenity_id)
-    if amenity is None:
-        abort(404)
+    amenities_list = []
+    if amenity_id is None:
+        all_amenities = storage.all(Amenity).values()
+        for value in all_amenities:
+            amenities_list.append(value.to_dict())
+        return jsonify(amenities_list)
+    else:
+        amenity = storage.get(Amenity, amenity_id)
+        if amenity is None:
+            abort(404)
+        return jsonify(amenity.to_dict())
 
-    return jsonify(amenity.to_dict())
 
-
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+@app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['DELETE'])
 def delete_amenity(amenity_id):
     """
         Deletes a Amenity object:
@@ -44,30 +42,32 @@ def delete_amenity(amenity_id):
         Args:
         amenity_id -- Amenity Id
     """
-    amenity = Amenity.query.get(amenity_id)
+    amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
 
-    amenity.delete()
+    storage.delete(amenity)
+    amenity.save()
     return jsonify({}), 200
 
 
-@app_views.route('/amenities', methods=['POST'])
+@app_views.route('/amenities', strict_slashes=False, methods=['POST'])
 def create_amenity():
     """
         Creates a Amenity: POST /api/v1/amenities
     """
-    if not request.json:
+    amenities_data = request.get_json(force=True, silent=True)
+    if not amenities_data:
         abort(400, 'Not a JSON')
-    if 'name' not in request.json:
+    if 'name' not in amenities_data:
         abort(400, 'Missing name')
 
-    amenity = Amenity(name=request.json['name'])
-    amenity.save()
-    return jsonify(amenity.to_dict()), 201
+    new_amenity = Amenity(**amenities_data)
+    new_amenity.save()
+    return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+@app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['PUT'])
 def update_amenity(amenity_id):
     """
         Updates a Amenity object: PUT /api/v1/amenities/<amenity_id>
@@ -75,14 +75,13 @@ def update_amenity(amenity_id):
         Args:
         amenity_id -- Amenity id to updated
     """
-    amenity = Amenity.query.get(amenity_id)
+    amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    if amenity not in request.json:
+    amenities_data = request.get_json(force=True, silent=True)
+    if amenity not in amenities_data:
         abort(400, 'Not a JSON')
 
-    for key, value in request.json.items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(amenity, key, value)
+    amenity.name = amenities_data.get("name", amenity.name)
     amenity.save()
-    return jsonify(amenity.to_dict())
+    return jsonify(amenity.to_dict()), 200
